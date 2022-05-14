bcbcdd340b7505d6536151f7e809f7cf429e2dec
Andi Faizal <andifaizal647@gmail.com>
2022-05-14 14:33:04 +0700

    add admin

--- src/routes/v1/admin.ts
+++ src/routes/v1/admin.ts
@@ -1,9 +1,9 @@
 import * as express from 'express';
-import * as FileController from '../../controllers/file.controller';
+import * as AdminController from '../../controllers/admin.controller';
 
 const router = express.Router();
 
-router.post('/admin/auth/login', FileController.store);
+router.post('/admin/auth/login', AdminController.store);
 // router.delete('/file/upload/:id', FileController.destroy);
 
 export default router;
--- src/routes/v1/adminCustomer.ts
+++ src/routes/v1/adminCustomer.ts
@@ -3,7 +3,7 @@ import * as adminCustomerController from '../../controllers/admin-customer.contr
 
 const router = express.Router();
 
-router.post('/admin/customer', adminCustomerController.store);
+router.post('/admin/customer/change-status', adminCustomerController.store);
 router.get('/admin/customer', adminCustomerController.index);
 
 export default router;
